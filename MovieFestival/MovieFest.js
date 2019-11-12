aed84dae0594377d03d53a55ebe7894ff0403384
MilanSunjevaric <56505160+MilanSunjevaric@users.noreply.github.com>
2019-11-12 17:53:02 +0100

    MOvie finish

--- MovieFestival/MovieFest.js
+++ MovieFestival/MovieFest.js
@@ -1,80 +1,100 @@
 
 function Genre(name) {
     this.name = name;
-
-    this.getData = function () {
-        return this.name;
-    }
 }
+Genre.prototype.getData = function () {
+    return this.name;
+}
+String.prototype.print = function () {
+    return ">> " + this + " <<";
+}
+var drama = new Genre("drama");
+var action = new Genre("action");
+var comedy = new Genre('comedy')
+
 function Movie(title, genre, length) {
     this.title = title;
     this.genre = genre;
     this.length = length;
-
-
-    this.getData = function () {
-        return 'Movie: ' + this.title + ', ' + 'Genre: ' + this.genre.getData() + ' Length: ' + this.length
-    }
 }
-var drama = new Genre("drama");
-var action = new Genre("action");
-var comedy = new Genre("comedy")
+Movie.prototype.getData = function () {
+    return 'Movie: ' + this.title + ', ' + 'Genre: ' + this.genre.getData() + ' Length: ' + this.length
+}
 
-var titanik = new Movie("titanik", drama, 210);
-var hunt = new Movie("hunt", drama, 110);
-var exam = new Movie("exam", drama, 150);
-var professional = new Movie("professional", drama, 230);
-
+var titanik = new Movie("Titanik", drama, 210);
+var hunt = new Movie("Hunt", drama, 110);
+var exam = new Movie("Exam", drama, 150);
+var professional = new Movie("Professional", drama, 230);
 
 function Program(date) {
 
     this.date = new Date(date);
     this.listOfMovies = [];
     this.numberOfMovies = 0;
-
-    this.addMovie = function (movie) {
-        this.listOfMovies.push(movie);
-        this.numberOfMovies++;
-
-        console.log(date + ', ' + '\n' + "Movie:", movie.getData());
-    }
-
-    this.getData = function () {
-        return 'Program: ' + this.date + ', ' + ' List of movies: ' + this.listOfMovies + 'Number of movies: ' + this.numberOfMovies;
-
-    }
 }
+
+Program.prototype.addMovie = function (movie) {
+
+    this.listOfMovies.push(movie);
+    this.numberOfMovies++;
+}
+
+Program.prototype.getProgramLength = function () {
+    var totalLength = 0;
+
+    for (var i = 0; i < this.listOfMovies.length; i++) {
+        var movie = this.listOfMovies[i];
+        totalLength += movie.length;
+    }
+    return totalLength;
+}
+
+Program.prototype.getData = function () {
+    var output = this.date + ', ' + this.getProgramLength() + ' min\n';
+    for (var i = 0; i < this.listOfMovies.length; i++) {
+        var movie = this.listOfMovies[i]
+        output += movie.getData() + '\n';
+    }
+    return output;
+}
+
 var day1 = new Program("01-01-2020");
 var day2 = new Program("02-01-2020");
-
-
-
 
 day1.addMovie(titanik);
 day1.addMovie(exam);
 day2.addMovie(hunt);
-day2.addMovie(professional);
+day2.addMovie(exam);
+
+day1.getData();
+day2.getData();
 
 function Festival(name) {
     this.name = name;
     this.listOfProgram = [];
     this.numberOfMoviesAll = 0;
-
-    this.addProgram = function (program) {
-        this.listOfProgram.push(program);
-        console.log(this.listOfProgram);
-    }
-    this.addNumOfAllMovies = function (num) {
-    }
 }
-
-var festival = new Festival("Cannes movie festival");
+Festival.prototype.addProgram = function (program) {
+    this.numberOfMoviesAll += program.listOfMovies.length
+    this.listOfProgram.push(program)
+}
+Festival.prototype.getFestivalData = function () {
+    var output = this.name + ' has ' + this.numberOfMoviesAll + ' movies titles \n';
+    for (var i = 0; i < this.listOfProgram.length; i++) {
+        var program = this.listOfProgram[i];
+        output += program.getData();
+    }
+    return output;
+}
+var festival = new Festival("Cannes movie festival,");
 
 festival.addProgram(day1);
-festival.addProgram(day2);
+festival.addProgram(day1);
 
-festival.addNumOfAllMovies(day1);
-festival.addNumOfAllMovies(day2);
+console.log("--------------");
+console.log(festival.getFestivalData().print());
+
+
 
 
 
